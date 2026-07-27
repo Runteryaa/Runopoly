12b5b7ed4fdd4e626771ba58697aa5076c0e1245
Runterya <runterya@example.com>
2026-07-27 22:43:08 +0300

    Fix socket.io transports for React Native

--- utils/socket.ts
+++ utils/socket.ts
@@ -4,5 +4,6 @@ import Constants from 'expo-constants';
 const SOCKET_URL = `http://150.230.154.153:3000`;
 
 export const socket = io(SOCKET_URL, {
-  autoConnect: false, 
+  autoConnect: false,
+  transports: ['websocket'],
 });
