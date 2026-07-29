a3cd38e818d4b877748fb31a2d94d6043b4a4764
Runterya <runterya@example.com>
2026-07-29 19:14:16 +0300

    Update SOCKET_URL to HTTPS for web support

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import Constants from 'expo-constants';
 
-const SOCKET_URL = `http://150.230.154.153:3000`;
+const SOCKET_URL = `https://runopoly.ddns.net`;
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
