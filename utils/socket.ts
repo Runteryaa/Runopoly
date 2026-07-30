9462ba55a6aa6fc71ebf63eb0f58784436366b9e
Runterya <97337724+Runteryaa@users.noreply.github.com>
2026-07-30 13:12:31 +0300

    Update socket.ts

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,8 +1,10 @@
 import { io } from 'socket.io-client';
 import Constants from 'expo-constants';
+import { Platform } from 'react-native';
 
 const SOCKET_URL = `https://runwatch.qzz.io`;
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
+  ...(Platform.OS !== 'web' ? { transports: ['websocket'] } : {})
 });
