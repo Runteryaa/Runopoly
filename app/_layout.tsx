1d3fbc935eb83e65b31c8efb24fcaaad2b08d085
Runterya <runterya@example.com>
2026-08-01 19:06:40 +0300

    feat: Add graceful shutdown handling and global server alerts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,19 @@
 import { Stack } from "expo-router";
 import "../global.css";
 import { CustomAlertModal } from "../components/CustomAlertModal";
+import { useEffect } from "react";
+import { socket } from "../utils/socket";
+import { CustomAlert } from "../utils/alert";
 
 export default function RootLayout() {
+  useEffect(() => {
+    socket.on('server_message', (msg) => {
+      CustomAlert.alert('Sistem Duyurusu', msg);
+    });
+    return () => {
+      socket.off('server_message');
+    };
+  }, []);
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
